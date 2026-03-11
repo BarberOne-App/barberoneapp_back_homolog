569803a967e91d519b97643328ed144ec1ad77c7
RodolphoBuettel <rodolpho05buettel@gmail.com>
2026-03-11 09:32:22 -0300

    console em barbeiro

--- src/controllers/barberController.ts
+++ src/controllers/barberController.ts
@@ -64,6 +64,8 @@ export async function updateBarber(req: Request, res: Response) {
   const b = UpdateBarberSchema.validate(req.body, { abortEarly: false });
   if (b.error) return res.status(422).send(joiErrors(b.error));
 
+  console.log("Update barber data:", req.body);
+
   const result = await updateBarberService({
     barbershopId: req.user!.barbershopId,
     actorRole: req.user!.role,
